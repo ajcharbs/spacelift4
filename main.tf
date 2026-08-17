73140c1e91c025e2abd589ea699383a9cfdff511
ajcharbs <46729757+ajcharbs@users.noreply.github.com>
2026-08-17 12:44:04 -0400

    Add AWS provider and S3 bucket configuration

--- main.tf
+++ main.tf
@@ -1,1 +1,30 @@
 
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "environment" {
+  default = "demo"
+}
+
+locals {
+  env = coalesce(var.environment, "unknown")
+}
+
+resource "aws_s3_bucket" "data" {
+  bucket_prefix = "orbit-labs-"
+
+  tags = {
+    name        = "Orbit Labs Storage"
+    managedBy   = "Spacelift"
+    environment = local.env
+  }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.data.id
+}
+
+output "deploy_env" {
+  value = local.env
+}
